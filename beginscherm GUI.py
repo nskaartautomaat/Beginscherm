977a204e0a25d682e0c8897f527bf947e6ea87bf
psychobears <kristiaan.hoogendoorn@hotmail.nl>
2015-10-26 14:24:20 +0100

    zo kan de api worden aangeroepen

--- beginscherm GUI.py
+++ beginscherm GUI.py
@@ -1,10 +1,7 @@
-__author__ = 'Kristiaan-laptop'
-fdkkekkekdekfsdfsdfsd
-f
-sdf
-asf
-asd
-fas
-dfa
-sdf
-poep
+import requests
+
+auth_details= ('kristiaan.hoogendoorn@hotmail.nl','rgQTyv_3EC9oFthMAvpQQBA0gnWxMH7PDATjqo3h8tbJGBOg5Y3-3w')
+
+response = requests.get('http://webservices.ns.nl/ns-api-avt?station=ut', auth=auth_details)
+
+print(response.text)
