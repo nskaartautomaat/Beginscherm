52aff562964b132ac7288fedf51400dfb0d4c099
KristiaanHoogendoorn <kristiaan.hoogendoorn@hotmail.nl>
2015-10-28 14:42:18 +0100

    beginscherm met actuele reistijd. layout nog niet goed

--- beginscherm GUI_CLASS.py
+++ beginscherm GUI_CLASS.py
@@ -1,15 +1,17 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
+import requests
+import xmltodict
 
 class Beginscherm:
 
     def __init__(self,master):
         window.configure(bg="#fdcf19")
+        #window.geometry("1024x680")
         window.geometry("1366x768")
 
         global MainFrame
-        MainFrame = Frame(master,bg="#fdcf19",height=718)
+        MainFrame = Frame(master,bg="#fdcf19",height=630)
         MainFrame.pack(side=TOP,fill=X)
 
         global BotFrame
@@ -23,7 +25,7 @@ class Beginscherm:
         self.l2.place(relx=0.5, rely=0.5,y=-100, anchor=CENTER)
 
         self.l3= Label (BotFrame,image=photo2,bg="#01236A")
-        self.l3.place(relx=0.5, rely=0.5,x=-180,anchor=E)
+        self.l3.place(relx=0.5, rely=0.5,y=5,anchor=E)
 
 
         self.b1= Button(MainFrame,text="Kopen \nlos kaartje",fg="white",bg="#01236A",width=19,height=3, font=("arial",15))
@@ -40,31 +42,194 @@ class Beginscherm:
     def bpress(self):
         MainFrame.destroy()
         BotFrame.destroy()
-        Test(window)
+        Treinen(window)
 
-class Test:
 
+class Treinen:
     def __init__(self,master):
-        frame = Frame(master)
-        frame.pack()
+        global frame2
+        frame2= Frame(master)
+        frame2.pack()
 
-        self.printButton = Button(frame, text="test msg",command=self.printMassage)
-        self.printButton.pack(side=LEFT)
+        self.labeltijd = Label(frame2,anchor='w',text='Tijd',fg="#003399",bg="#fdcf19", width=5).grid(row=0,column=0)
+        self.LabelVertraging = Label(frame2,anchor='w',text='',fg="#003399",bg="#fdcf19", width=4).grid(row=0,column=1)
+        self.labelnaar = Label(frame2,anchor='w',text='Naar', fg="#003399",bg="#fdcf19", width=25).grid(row=0,column=2)
+        self.labelvertrekspoor = Label(frame2,anchor='w',text='Spoor', fg="#003399",bg="#fdcf19", width=12).grid(row=0,column=3)
+        self.labelroutetekst = Label(frame2,anchor='w',text='Via', fg="#003399",bg="#fdcf19", width=60).grid(row=0,column=4)
+        self.labelvervoerder = Label(frame2,anchor='w',text='Vervoerder', fg="#003399",bg="#fdcf19", width=18).grid(row=0,column=5)
+        self.labelritnummer = Label(frame2,anchor='w',text='Ritnummer', fg="#003399",bg="#fdcf19", width=15).grid(row=0,column=6)
+        self.labeltreinsoort = Label(frame2,anchor='w',text='Treinsoort', fg="#003399",bg="#fdcf19", width=15).grid(row=0,column=7)
 
-        self.quitButton = Button (frame, text="Quit",command=frame.quit)
-        self.quitButton.pack(side=LEFT)
+        for hoeveelstetrein in range(1, aantaltreinen,2):
+            self.tijd = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekTijd'][11:16]
+            self.Naar = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['EindBestemming']
+            self.Ritnummer = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RitNummer']
+            self.Treinsoort = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['TreinSoort']
+            self.Vertrekspoor = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekSpoor']['#text']
 
-    def printMassage(self):
-        print("dit is een bericht")
+            try:
+                self.Vervoerder = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['Vervoerder']
+            except KeyError:
+                self.Vervoerder = ''
+            try:
+                self.Vertraging = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekVertragingTekst'][0:2]
+            except KeyError:
+                self.Vertraging = ''
+
+            try:
+                self.Via = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Via = ''
+
+            try:
+                self.Routetekst = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Routetekst = ''
+
+            self.labeltijdAPI = Label(frame2,anchor='w',text=self.tijd,fg="white",bg="#003399", width=5).grid(row=hoeveelstetrein,column=0)
+            self.labelVertraging = Label(frame2,anchor='w',text=self.Vertraging,fg="red",bg="#003399", width=4).grid(row=hoeveelstetrein,column=1)
+            self.labelnaarAPI = Label(frame2,anchor='w',text=self.Naar, fg="white",bg="#003399", width=25).grid(row=hoeveelstetrein,column=2)
+            self.labelvertrekspoorAPI = Label(frame2,anchor='w',text=self.Vertrekspoor, fg="white",bg="#003399", width=12).grid(row=hoeveelstetrein,column=3)
+            self.labelroutetekstAPI = Label(frame2,anchor='w',text=self.Routetekst, fg="white",bg="#003399", width=60).grid(row=hoeveelstetrein,column=4)
+            self.labelvervoerderAPI = Label(frame2,anchor='w',text=self.Vervoerder, fg="white",bg="#003399", width=18).grid(row=hoeveelstetrein,column=5)
+            self.labelritnummerAPI = Label(frame2,anchor='w',text=self.Ritnummer, fg="white",bg="#003399", width=15).grid(row=hoeveelstetrein,column=6)
+            self.labeltreinsoortAPI = Label(frame2,anchor='w',text=self.Treinsoort, fg="white",bg="#003399", width=15).grid(row=hoeveelstetrein,column=7)
+
+        for hoeveelstetrein in range(2, aantaltreinen,2):
+            self.tijd = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekTijd'][11:16]
+            self.Naar = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['EindBestemming']
+            self.Ritnummer = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RitNummer']
+            self.Treinsoort = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['TreinSoort']
+            self.Vertrekspoor = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekSpoor']['#text']
+
+
+            try:
+                self.Vervoerder = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['Vervoerder']
+            except KeyError:
+                self.Vervoerder = ''
+            try:
+                self.Vertraging = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekVertragingTekst'][0:2]
+            except KeyError:
+                self.Vertraging = ''
+
+            try:
+                self.Via = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Via = ''
+
+            try:
+                self.Routetekst = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Routetekst = ''
+
+            self.labeltijdAPI = Label(frame2,anchor='w',text=self.tijd,fg="white",bg="#0000ff", width=5).grid(row=hoeveelstetrein,column=0)
+            self.labelVertraging = Label(frame2,anchor='w',text=self.Vertraging,fg="red",bg="#0000ff", width=4).grid(row=hoeveelstetrein,column=1)
+            self.labelnaarAPI = Label(frame2,anchor='w',text=self.Naar, fg="white",bg="#0000ff", width=25).grid(row=hoeveelstetrein,column=2)
+            self.labelvertrekspoorAPI = Label(frame2,anchor='w',text=self.Vertrekspoor, fg="white",bg="#0000ff", width=12).grid(row=hoeveelstetrein,column=3)
+            self.labelroutetekstAPI = Label(frame2,anchor='w',text=self.Routetekst, fg="white",bg="#0000ff", width=60).grid(row=hoeveelstetrein,column=4)
+            self.labelvervoerderAPI = Label(frame2,anchor='w',text=self.Vervoerder, fg="white",bg="#0000ff", width=18).grid(row=hoeveelstetrein,column=5)
+            self.labelritnummerAPI = Label(frame2,anchor='w',text=self.Ritnummer, fg="white",bg="#0000ff", width=15).grid(row=hoeveelstetrein,column=6)
+            self.labeltreinsoortAPI = Label(frame2,anchor='w',text=self.Treinsoort, fg="white",bg="#0000ff", width=15).grid(row=hoeveelstetrein,column=7)
+
+            self.labelLeeg = Label(frame2,text = '',bg="#fdcf19").grid(row=aantaltreinen+1,column=2)
+            self.buttonMeerResultaten = Button(frame2,text='Meer \n treinen', fg="white",bg="#01236A",width=19,height=3, font=("arial",15), command=self.meerResult).place(relx=0.5, rely=0.5,y=-355,x=450)
+            self.buttonTerug = Button(frame2,text='Terug', fg="white",bg="#01236A",width=19,height=3,command=self.terug,font=("arial",15)).place(relx=0.5, rely=0.5,y=-260,x=450)
+
+    def meerResult(self):
+        for hoeveelstetrein in range(aantaltreinen+1, aantaltreinen+5,2):
+            self.tijd = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekTijd'][11:16]
+            self.Naar = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['EindBestemming']
+            self.Ritnummer = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RitNummer']
+            self.Treinsoort = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['TreinSoort']
+            self.Vertrekspoor = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekSpoor']['#text']
+
+            try:
+                self.Vervoerder = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['Vervoerder']
+            except KeyError:
+                self.Vervoerder = ''
+            try:
+                self.Vertraging = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekVertragingTekst'][0:2]
+            except KeyError:
+                self.Vertraging = ''
+
+            try:
+                self.Via = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Via = ''
+
+            try:
+                self.Routetekst = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Routetekst = ''
+
+            self.labeltijdAPI = Label(frame2,anchor='w',text=self.tijd,fg="white",bg="#003399", width=5).grid(row=hoeveelstetrein,column=0)
+            self.labelVertraging = Label(frame2,anchor='w',text=self.Vertraging,fg="red",bg="#003399", width=4).grid(row=hoeveelstetrein,column=1)
+            self.labelnaarAPI = Label(frame2,anchor='w',text=self.Naar, fg="white",bg="#003399", width=25).grid(row=hoeveelstetrein,column=2)
+            self.labelvertrekspoorAPI = Label(frame2,anchor='w',text=self.Vertrekspoor, fg="white",bg="#003399", width=12).grid(row=hoeveelstetrein,column=3)
+            self.labelroutetekstAPI = Label(frame2,anchor='w',text=self.Routetekst, fg="white",bg="#003399", width=60).grid(row=hoeveelstetrein,column=4)
+            self.labelvervoerderAPI = Label(frame2,anchor='w',text=self.Vervoerder, fg="white",bg="#003399", width=18).grid(row=hoeveelstetrein,column=5)
+            self.labelritnummerAPI = Label(frame2,anchor='w',text=self.Ritnummer, fg="white",bg="#003399", width=15).grid(row=hoeveelstetrein,column=6)
+            self.labeltreinsoortAPI = Label(frame2,anchor='w',text=self.Treinsoort, fg="white",bg="#003399", width=15).grid(row=hoeveelstetrein,column=7)
+            self.labelLeeg = Label(text = '',bg="#fdcf19").grid(row=hoeveelstetrein,column=2)
+
+        for hoeveelstetrein in range(aantaltreinen, aantaltreinen+5,2):
+            self.tijd = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekTijd'][11:16]
+            self.Naar = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['EindBestemming']
+            self.Ritnummer = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RitNummer']
+            self.Treinsoort = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['TreinSoort']
+            self.Vertrekspoor = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekSpoor']['#text']
+
+
+            try:
+                self.Vervoerder = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['Vervoerder']
+            except KeyError:
+                self.Vervoerder = ''
+            try:
+                self.Vertraging = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['VertrekVertragingTekst'][0:2]
+            except KeyError:
+                self.Vertraging = ''
+
+            try:
+                self.Via = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Via = ''
+
+            try:
+                self.Routetekst = xmldi['ActueleVertrekTijden']['VertrekkendeTrein'][hoeveelstetrein]['RouteTekst']
+            except KeyError:
+                self.Routetekst = ''
+
+            self.labeltijdAPI = Label(frame2,anchor='w',text=self.tijd,fg="white",bg="#0000ff", width=5).grid(row=hoeveelstetrein,column=0)
+            self.labelVertraging = Label(frame2,anchor='w',text=self.Vertraging,fg="red",bg="#0000ff", width=4).grid(row=hoeveelstetrein,column=1)
+            self.labelnaarAPI = Label(frame2,anchor='w',text=self.Naar, fg="white",bg="#0000ff", width=25).grid(row=hoeveelstetrein,column=2)
+            self.labelvertrekspoorAPI = Label(frame2,anchor='w',text=self.Vertrekspoor, fg="white",bg="#0000ff", width=12).grid(row=hoeveelstetrein,column=3)
+            self.labelroutetekstAPI = Label(frame2,anchor='w',text=self.Routetekst, fg="white",bg="#0000ff", width=60).grid(row=hoeveelstetrein,column=4)
+            self.labelvervoerderAPI = Label(frame2,anchor='w',text=self.Vervoerder, fg="white",bg="#0000ff", width=18).grid(row=hoeveelstetrein,column=5)
+            self.labelritnummerAPI = Label(frame2,anchor='w',text=self.Ritnummer, fg="white",bg="#0000ff", width=15).grid(row=hoeveelstetrein,column=6)
+            self.labeltreinsoortAPI = Label(frame2,anchor='w',text=self.Treinsoort, fg="white",bg="#0000ff", width=15).grid(row=hoeveelstetrein,column=7)
+
+        global aantaltreinen
+        aantaltreinen +=5
+    def terug(self):
+        frame2.destroy()
+        Beginscherm(window)
 
 
 try:
     window= Tk()
 
+    url = 'http://webservices.ns.nl/ns-api-avt?station=ut'
+    user = 'dennis-strik@hotmail.com'
+    passw = 'Bmz5NUzoLz3GLtbRrbwIznZUdHs_5pqL5Yv-6dMrW5V_lknjcVqStg'
+    response = requests.get(url, auth=(user, passw))
+    xmldi = xmltodict.parse(response.text)
+    hoeveelstetrein = 1
+    aantaltreinen = 16
+
     photo1 = ImageTk.PhotoImage(Image.open ("ovkaart.jpg"))
     photo2 = ImageTk.PhotoImage(Image.open ("taalenopties.jpg"))
 
-    beginscherm = Beginscherm(window)
+    Beginscherm(window)
 
 
     window.mainloop()
